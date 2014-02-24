63caf5a8604339c033533e9b60da2f6d7c947f48
Armando Leon <armando@tapshield.com>
2014-02-24 13:06:23 -0500

    Added call to parent activity on event of onItemClick

--- src/com/tapshield/android/ui/fragment/NavigationFragment.java
+++ src/com/tapshield/android/ui/fragment/NavigationFragment.java
@@ -7,14 +7,19 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.tapshield.android.R;
 
-public class NavigationFragment extends Fragment {
+public class NavigationFragment extends Fragment implements OnItemClickListener {
 
+	private ImageView mProfile;
 	private ListView mList;
 	private List<String> mListItems = new ArrayList<String>();
 	
@@ -22,12 +27,21 @@ public class NavigationFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_navigation, container, false);
 		mList = (ListView) view.findViewById(R.id.fragment_navigation_list);
+		mProfile = (ImageView) view.findViewById(R.id.fragment_navigation_image);
 		return view;
 	}
 	
 	@Override
 	public void onStart() {
 		super.onStart();
+		
+		mProfile.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				onItemClick(null,  null, 100, view.getId());
+			}
+		});
 		
 		mListItems.add("Crime Data");
 		mListItems.add("Safe Circle");
@@ -40,6 +54,18 @@ public class NavigationFragment extends Fragment {
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
 				R.layout.item_navigation, mListItems);
 		mList.setAdapter(adapter);
+		mList.setOnItemClickListener(this);
+		mList.setSelected(true);
+		mList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+		OnNavigationItemClickListener listener = (OnNavigationItemClickListener) getActivity();
+		listener.onNavigationItemClick(position);
 	}
 	
+	public interface OnNavigationItemClickListener {
+		void onNavigationItemClick(int position);
+	}
 }
