916da1611028cf7d4049c03edcd3f7f84ff30f58
Armando Leon <armando@tapshield.com>
2014-02-24 12:58:38 -0500

    Added basic map and navigation drawer functionality

--- src/com/tapshield/android/ui/activity/MainActivity.java
+++ src/com/tapshield/android/ui/activity/MainActivity.java
@@ -1,19 +1,44 @@
 package com.tapshield.android.ui.activity;
 
-import com.tapshield.android.R;
-
 import android.app.ActionBar;
-import android.app.Activity;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.tapshield.android.R;
+import com.tapshield.android.api.JavelinClient;
+import com.tapshield.android.app.TapShieldApplication;
+import com.tapshield.android.location.LocationTracker;
+import com.tapshield.android.ui.fragment.NavigationFragment.OnNavigationItemClickListener;
+import com.tapshield.android.utils.UiUtils;
 
+public class MainActivity extends FragmentActivity implements OnNavigationItemClickListener,
+		LocationListener {
+
+	private static final String KEY_RESUME = "resuming";
+	
 	private DrawerLayout mDrawerLayout;
 	private FrameLayout mDrawer;
+	private GoogleMap mMap;
+	private Circle mAccuracyBubble, mUser;
+	
+	private JavelinClient mJavelin;
+	private LocationTracker mTracker;
+	
+	private boolean mResuming = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +47,50 @@ public class MainActivity extends Activity {
 		
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
 		mDrawer = (FrameLayout) findViewById(R.id.main_drawer);
+		mMap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.main_fragment_map)).getMap();
+		
+		mJavelin = JavelinClient.getInstance(this, TapShieldApplication.JAVELIN_CONFIG);
+		mTracker = LocationTracker.getInstance(this);
 		
 		ActionBar actionBar = getActionBar();
 		actionBar.setHomeButtonEnabled(true);
+		
+		if (savedInstanceState != null) {
+			mResuming = savedInstanceState.getBoolean(KEY_RESUME);
+		}
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putBoolean(KEY_RESUME, mResuming);
+		super.onSaveInstanceState(outState);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		boolean userPresent = mJavelin.getUserManager().isPresent();
+		
+		if (!userPresent) {
+			if (mResuming) {
+				//if resuming but still no user, login was not successful, finish it
+				finish();
+			} else {
+				mResuming = true;
+				UiUtils.startActivityNoStack(this, WelcomeActivity.class);
+			}
+		}
+		mTracker.start();
+		mTracker.addLocationListener(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mTracker.removeLocationListener(this);
+		mTracker.stop();
 	}
 
 	@Override
@@ -46,5 +112,56 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return false;
+	}
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		float maxZoom = mMap.getMaxZoomLevel();
+		float standardZoom = 18;
+		if (standardZoom > maxZoom) {
+			standardZoom = maxZoom;
+		}
+		float currentZoom = mMap.getCameraPosition().zoom;
+		float newZoom = currentZoom < standardZoom ? standardZoom : currentZoom;
+
+		//animate camera
+		LatLng cameraLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+		CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(cameraLatLng, newZoom);
+		mMap.animateCamera(cameraUpdate);
+
+		//accuracy
+		LatLng center = cameraLatLng;
+		float radius = location.getAccuracy();
+
+		if (mAccuracyBubble == null) {
+			CircleOptions bubbleOptions = new CircleOptions()
+					.center(center)
+					.radius(radius)
+					.strokeWidth(4)
+					.strokeColor(Color.parseColor("#FF009966"))
+					.fillColor(Color.parseColor("#33009966"));
+
+			mAccuracyBubble = mMap.addCircle(bubbleOptions);
+		} else {
+			mAccuracyBubble.setCenter(center);
+			mAccuracyBubble.setRadius(radius);
+		}
+		
+		//user
+		if (mUser == null) {
+			CircleOptions userOptions = new CircleOptions()
+					.center(center)
+					.radius(1)
+					.strokeWidth(0)
+					.fillColor(Color.parseColor("#FF009999"));
+			mUser = mMap.addCircle(userOptions);
+		} else {
+			mUser.setCenter(center);
+		}
+	}
+
+	@Override
+	public void onNavigationItemClick(int position) {
+		
 	}
 }
