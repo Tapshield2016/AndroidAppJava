f6f745ba02ebc8e2dc1f78ee2e0280a76d7dacba
Armando Leon <armando@tapshield.com>
2014-03-06 13:26:30 -0500

    Added adapter for mass alerts

---
+++ src/com/tapshield/android/ui/adapter/MassAlertAdapter.java
@@ -0,0 +1,99 @@
+package com.tapshield.android.ui.adapter;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.tapshield.android.api.model.MassAlert;
+
+public class MassAlertAdapter extends BaseAdapter {
+
+	private Context mContext;
+	private int mResource;
+	private int mMessageResource;
+	private int mDateTimeResource;
+	private String mDateTimeFormatPattern = "hh:mm:ss aa MMMM dd yyyy";
+	private List<MassAlert> mItems;
+	private LayoutInflater mLayoutInflater;
+	
+	public MassAlertAdapter(Context context, int layoutResource, int messageTextResourceId,
+			int dateTimeTextResourceId, List<MassAlert> items) {
+		mContext = context;
+		mResource = layoutResource;
+		mMessageResource = messageTextResourceId;
+		mDateTimeResource = dateTimeTextResourceId;
+		mItems = items;
+		
+		if (mItems == null) {
+			mItems = Collections.emptyList();
+		}
+	}
+	
+	public void setDateTimePattern(String newPattern) {
+		mDateTimeFormatPattern = newPattern;
+	}
+	
+	public void setItems(List<MassAlert> items) {
+		mItems = items;
+		notifyDataSetChanged();
+	}
+	
+	public void setItemsNoNotifyDataSetChanged(List<MassAlert> items) {
+		mItems = items;
+	}
+	
+	@Override
+	public int getCount() {
+		return mItems.size();
+	}
+
+	@Override
+	public MassAlert getItem(int position) {
+		return mItems.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+
+		if (view == null) {
+			if (mLayoutInflater == null) {
+				mLayoutInflater = LayoutInflater.from(mContext);
+
+				//if still null, retrieve via context
+				if (mLayoutInflater == null) {
+					mLayoutInflater = (LayoutInflater)
+							mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				}
+			}
+
+			view = mLayoutInflater.inflate(mResource, null);
+		}
+
+		MassAlert alert = getItem(position);
+		TextView message = (TextView) view.findViewById(mMessageResource);
+		TextView time = (TextView) view.findViewById(mDateTimeResource);
+		
+		DateTime utcDate = new DateTime(alert.getTimestampInSeconds() * 1000, DateTimeZone.UTC);
+		DateTime currentDate = utcDate.toDateTime(DateTimeZone.getDefault());
+		
+		message.setText(alert.message);
+		time.setText(currentDate.toString(mDateTimeFormatPattern));
+		
+		return view;
+	}
+}
