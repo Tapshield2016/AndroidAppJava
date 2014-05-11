9c52426e83f78c11b97ab9d316451c74a2ce2db1
Armando Leon <armando@tapshield.com>
2014-05-11 16:08:47 -0400

    Updated reference of report types to their drawables

--- src/com/tapshield/android/utils/SocialReportsUtils.java
+++ src/com/tapshield/android/utils/SocialReportsUtils.java
@@ -8,17 +8,18 @@ public class SocialReportsUtils {
 
 	//check javelinsocialreportingmanager for excluded types
 	public static final int[] SOCIAL_REPORTS_DRAWABLES_ID = {
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done,
-		R.drawable.ic_actionbar_done
+		//R.drawable.ic_launcher,
+		R.drawable.ts_report_assault,
+		R.drawable.ts_report_brokenbone,
+		//R.drawable.ic_launcher,
+		R.drawable.ts_report_highfever,
+		R.drawable.ts_report_choking,
+		R.drawable.ts_report_other,
+		//R.drawable.ic_launcher,
+		//R.drawable.ic_launcher,
+		R.drawable.ts_report_burglary,
+		R.drawable.ts_report_theft,
+		R.drawable.ts_report_vandalism
 	};
 	
 	public final static int getImageResourceByType(String type) {
