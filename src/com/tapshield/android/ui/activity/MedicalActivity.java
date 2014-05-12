00dce550cf69056be7959f2e0ce3580d3741fbb2
Armando Leon <armando@tapshield.com>
2014-05-12 12:10:51 -0400

    Added MedicalActivity

---
+++ src/com/tapshield/android/ui/activity/MedicalActivity.java
@@ -0,0 +1,82 @@
+package com.tapshield.android.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.tapshield.android.R;
+import com.tapshield.android.api.JavelinClient;
+import com.tapshield.android.api.JavelinUserManager;
+import com.tapshield.android.api.model.User;
+import com.tapshield.android.api.model.UserProfile;
+import com.tapshield.android.app.TapShieldApplication;
+
+public class MedicalActivity extends Activity {
+
+	private JavelinUserManager mUserManager;
+	private EditText mAllergies;
+	private EditText mMedication;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile_medical);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		mUserManager = JavelinClient
+				.getInstance(this, TapShieldApplication.JAVELIN_CONFIG)
+				.getUserManager();
+		
+		mAllergies = (EditText) findViewById(R.id.profile_medical_edit_allergies);
+		mMedication = (EditText) findViewById(R.id.profile_medical_edit_medication);
+		
+		load();
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public void onBackPressed() {
+		save();
+	}
+	
+	private void load() {
+		User user = mUserManager.getUser();
+		UserProfile profile = user.profile;
+		
+		if (profile.hasAllergies()) {
+			mAllergies.setText(profile.getAllergies());
+		}
+		
+		if (profile.hasMedications()) {
+			mAllergies.setText(profile.getMedications());
+		}
+	}
+	
+	private void save() {
+		UserProfile profile = mUserManager.getUser().profile;
+		String allergies = mAllergies.getText().toString().trim();
+		String medication = mMedication.getText().toString().trim();
+
+		if (!allergies.isEmpty()) {
+			profile.setAllergies(allergies);
+		}
+		
+		if (!medication.isEmpty()) {
+			profile.setMedications(medication);
+		}
+		
+		mUserManager.setUserProfile(profile);
+		super.onBackPressed();
+	}
+}
