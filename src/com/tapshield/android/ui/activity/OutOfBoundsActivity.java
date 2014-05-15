3ba089f880210a7622714419655ec4cd19d52202
Armando Leon <armando@tapshield.com>
2014-05-15 02:26:28 -0400

    Fixed the simple layout for OutOfBoundsActivity

--- src/com/tapshield/android/ui/activity/OutOfBoundsActivity.java
+++ src/com/tapshield/android/ui/activity/OutOfBoundsActivity.java
@@ -8,31 +8,37 @@ import android.widget.Button;
 
 import com.tapshield.android.R;
 import com.tapshield.android.api.JavelinClient;
+import com.tapshield.android.api.model.Agency;
 import com.tapshield.android.app.TapShieldApplication;
 import com.tapshield.android.utils.UiUtils;
 
 public class OutOfBoundsActivity extends Activity implements OnClickListener {
 
+	private String mSecondaryPhone;
 	private Button mButton;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_outofbounds);
+		
+		Agency agency = JavelinClient
+				.getInstance(this, TapShieldApplication.JAVELIN_CONFIG)
+				.getUserManager()
+				.getUser()
+				.agency;
+		
+		mSecondaryPhone = agency != null ?
+				agency.secondaryNumber : getString(R.string.ts_no_org_emergency_number);
+		
 		mButton = (Button) findViewById(R.id.outofbounds_button);
 		mButton.setOnClickListener(this);
+		mButton.setText("Dial " + mSecondaryPhone);
 	}
 
 	@Override
 	public void onClick(View view) {
-		String secondaryNumber = JavelinClient
-				.getInstance(this, TapShieldApplication.JAVELIN_CONFIG)
-				.getUserManager()
-				.getUser()
-				.agency
-				.secondaryNumber;
-		
-		UiUtils.MakePhoneCall(this, secondaryNumber);
+		UiUtils.MakePhoneCall(this, mSecondaryPhone);
 		UiUtils.startActivityNoStack(this, MainActivity.class);
 	}
 }
