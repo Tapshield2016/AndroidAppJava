8b4298d89c0b5f3b1130c2e26872b8d1b37935c0
Armando Leon <armando@tapshield.com>
2014-05-28 15:11:36 -0400

    Updated Google Analytics required setup with version 4.4 (May 2014)

--- src/com/tapshield/android/ui/activity/BaseFragmentActivity.java
+++ src/com/tapshield/android/ui/activity/BaseFragmentActivity.java
@@ -1,20 +1,29 @@
 package com.tapshield.android.ui.activity;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.analytics.tracking.android.EasyTracker;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.tapshield.android.app.TapShieldApplication;
 
 public class BaseFragmentActivity extends FragmentActivity {
 
 	@Override
+	protected void onCreate(Bundle arg0) {
+		//Get a Tracker (should auto-report)
+		((TapShieldApplication) getApplication()).getTracker(TapShieldApplication.TrackerName.APP_TRACKER);
+		super.onCreate(arg0);
+	}
+	
+	@Override
 	protected void onStart() {
-		EasyTracker.getInstance(this).activityStart(this);
+		GoogleAnalytics.getInstance(this).reportActivityStart(this);
 		super.onStart();
 	}
 	
 	@Override
 	protected void onStop() {
-		EasyTracker.getInstance(this).activityStop(this);
+		GoogleAnalytics.getInstance(this).reportActivityStop(this);
 		super.onStop();
 	}
 }
