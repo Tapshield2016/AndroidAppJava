ec2bff46b801357c03bd25837b33e2c23daf68e9
Armando Leon <armando@tapshield.com>
2014-07-23 09:41:33 -0400

    Added alternative disarm mechanism by entering the password when prompting the user after 4 or more wrong attempts of disarm via passcode

--- src/com/tapshield/android/ui/fragment/DialpadFragment.java
+++ src/com/tapshield/android/ui/fragment/DialpadFragment.java
@@ -1,13 +1,19 @@
 package com.tapshield.android.ui.fragment;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.tapshield.android.R;
 import com.tapshield.android.api.JavelinClient;
+import com.tapshield.android.api.JavelinUserManager;
+import com.tapshield.android.api.model.User;
 import com.tapshield.android.app.TapShieldApplication;
 import com.tapshield.android.manager.EmergencyManager;
 import com.tapshield.android.manager.TwilioManager;
@@ -26,6 +32,11 @@ public class DialpadFragment extends Fragment implements DialpadListener {
 	private Dialpad mDialpad;
 	
 	private int[] mProgressColors;
+
+	private ProgressDialog mWorkingDialog;
+	private AlertDialog mForgotPasscodeDialog;
+	private EditText mPassword;
+	private int mWrongInputCount = 0;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,6 +56,9 @@ public class DialpadFragment extends Fragment implements DialpadListener {
 		mProgressColors = getResources().getIntArray(R.array.ts_background_alert_countdown);
 		mProgress.setListener((AnimatedVerticalColorProgress.Listener) getActivity());
 		mDialpad.setDialpadListener(this);
+		
+		mWorkingDialog = getWorkingDialog();
+		mForgotPasscodeDialog = getForgotPasscodeDialog();
 	}
 	
 	@Override
@@ -75,18 +89,99 @@ public class DialpadFragment extends Fragment implements DialpadListener {
 	public void onInputComplete(String input) {
 		//By default the Dialpad view has a limit of 4 entries
 		if (input.equals(mJavelin.getUserManager().getUser().getDisarmCode())) {
-			TwilioManager.getInstance(getActivity()).notifyEnd();
-			mEmergencyManager.cancel();
-			UiUtils.startActivityNoStack(getActivity(), MainActivity.class);
+			end();
 		} else {
 			mDialpad.setError(R.string.ts_dialpad_message_error);
+			mWrongInputCount++;
+			if (mWrongInputCount >= 4) {
+				mForgotPasscodeDialog.show();
+			}
 		}
 	}
+	
+	@Override
+	public void onInputChange(String newInput) {}
 	
 	public final void notifyAlertStarted() {
 		mProgress.end();
 	}
+	
+	private ProgressDialog getWorkingDialog() {
+		ProgressDialog dialog = new ProgressDialog(getActivity());
+		dialog.setMessage("Working. Please wait...");
+		dialog.setCancelable(false);
+		return dialog;
+	}
+	
+	private AlertDialog getForgotPasscodeDialog() {
+		
+		View content = LayoutInflater
+				.from(getActivity())
+				.inflate(R.layout.dialog_dialpad_forgotpasscode, null);
+		
+		mPassword = (EditText) content.findViewById(R.id.dialog_dialpad_forgotpasscode_edit);
+		
+		return new AlertDialog.Builder(getActivity())
+				.setTitle("Forgot your passcode?")
+				.setView(content)
+				.setCancelable(true)
+				.setPositiveButton("Disarm", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						if (mJavelin
+								.getUserManager()
+								.getUser()
+								.equalsPassword(mPassword.getText().toString())) {
+							end();
+						} else {
+							//there is a chance the user reset their password, re-login is required
+							mDialpad.postDelayed(new Runnable() {
+								
+								@Override
+								public void run() {
+									mForgotPasscodeDialog.show();
+									mPassword.setError("Wrong password");
+								}
+							}, 750);
+						}
+					}
+				})
+				.setNeutralButton("Reset Password", new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						mWorkingDialog.show();
+						mJavelin.sendPasswordResetEmail(mJavelin.getUserManager().getUser().email,
+								new JavelinClient.OnRequestPasswordResetListener() {
+									
+									@Override
+									public void onRequestPasswordReset(boolean successful, Throwable e) {
+										mWorkingDialog.dismiss();
 
-	@Override
-	public void onInputChange(String newInput) {}
+										if (successful) {
+											UiUtils.toastShort(getActivity(), "Email sent. Check your inbox!");
+											mJavelin.getUserManager().logOut(new JavelinUserManager.OnUserLogOutListener() {
+												
+												@Override
+												public void onUserLogOut(boolean successful, Throwable e) {
+													end();
+												}
+											});
+										} else {
+											//show again if unsuccessful request to reset password
+											mForgotPasscodeDialog.show();
+										}
+									}
+								});
+					}
+				})
+				.create();
+	}
+	
+	private void end() {
+		TwilioManager.getInstance(getActivity()).notifyEnd();
+		mEmergencyManager.cancel();
+		UiUtils.startActivityNoStack(getActivity(), MainActivity.class);
+	}
 }
