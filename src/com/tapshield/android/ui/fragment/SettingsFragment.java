3df5eb68eb90f304f277398456e854a7640ac1c8
Armando Leon <armando@tapshield.com>
2014-09-02 16:35:58 -0400

    Added settings entry for adding or changing the organization

--- src/com/tapshield/android/ui/fragment/SettingsFragment.java
+++ src/com/tapshield/android/ui/fragment/SettingsFragment.java
@@ -22,11 +22,13 @@ import com.tapshield.android.utils.UiUtils;
 public class SettingsFragment extends PreferenceFragment
 		implements OnPreferenceClickListener, OnUserLogOutListener {
 
+	private Preference mAddChangeOrg;
 	private Preference mChangePasscode;
 	private Preference mSignOut;
 	private Preference mFeedback;
 	private Preference mAbout;
 	
+	private String mAddChangeOrgKey;
 	private String mChangePasscodeKey;
 	private String mSignOutKey;
 	private String mFeedbackKey;
@@ -37,16 +39,19 @@ public class SettingsFragment extends PreferenceFragment
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
 		
+		mAddChangeOrgKey = getString(R.string.ts_settings_org_key);
 		mChangePasscodeKey = getString(R.string.ts_settings_passcode_key);
 		mSignOutKey = getString(R.string.ts_settings_logout_key);
 		mFeedbackKey = getString(R.string.ts_settings_feedback_key);
 		mAboutKey = getString(R.string.ts_settings_about_key);
 		
+		mAddChangeOrg = (Preference) getPreferenceManager().findPreference(mAddChangeOrgKey);
 		mChangePasscode = (Preference) getPreferenceManager().findPreference(mChangePasscodeKey);
 		mSignOut = (Preference) getPreferenceManager().findPreference(mSignOutKey);
 		mFeedback = (Preference) getPreferenceManager().findPreference(mFeedbackKey);
 		mAbout = (Preference) getPreferenceManager().findPreference(mAboutKey);
 		
+		mAddChangeOrg.setOnPreferenceClickListener(this);
 		mChangePasscode.setOnPreferenceClickListener(this);
 		mSignOut.setOnPreferenceClickListener(this);
 		mFeedback.setOnPreferenceClickListener(this);
@@ -56,14 +61,18 @@ public class SettingsFragment extends PreferenceFragment
 	@Override
 	public boolean onPreferenceClick(Preference preference) {
 		String key = preference.getKey();
+		boolean org = key.equals(mAddChangeOrgKey);
 		boolean change = key.equals(mChangePasscodeKey);
 		boolean signout = key.equals(mSignOutKey);
 		boolean feedback = key.equals(mFeedbackKey);
 		boolean about = key.equals(mAboutKey);
 		
-		if (change) {
-			Intent intent = new Intent(getActivity(), ResetPasscodePasswordActivity.class);
-			startActivity(intent);
+		Intent intent = null;
+		
+		if (org) {
+			intent = new Intent(getActivity(), SetOrganizationActivity.class);
+		} else if (change) {
+			intent = new Intent(getActivity(), ResetPasscodePasswordActivity.class);
 		} else if (signout) {
 			SessionManager
 					.getInstance(getActivity())
@@ -76,14 +85,18 @@ public class SettingsFragment extends PreferenceFragment
 			String email = getString(R.string.ts_misc_feedback_email);
 			String subject = getString(R.string.ts_misc_feedback_subject) + " (" + getVersion() + ")";
 			
-			Intent intent = new Intent(Intent.ACTION_SEND);
-			intent.setData(Uri.parse("mailto:"));
-			intent.setType("message/rfc822");
-			intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
-			intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-			startActivity(Intent.createChooser(intent, "Choose an Email client:"));
+			Intent send = new Intent(Intent.ACTION_SEND);
+			send.setData(Uri.parse("mailto:"));
+			send.setType("message/rfc822");
+			send.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+			send.putExtra(Intent.EXTRA_SUBJECT, subject);
+			
+			intent = Intent.createChooser(send, "Choose an Email client:");
 		} else if (about) {
-			Intent intent = new Intent(getActivity(), AboutActivity.class);
+			intent = new Intent(getActivity(), AboutActivity.class);
+		}
+		
+		if (intent != null) {
 			startActivity(intent);
 		}
 			
